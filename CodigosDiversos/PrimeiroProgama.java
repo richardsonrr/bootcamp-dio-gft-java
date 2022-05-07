11a6897b655085223cf22409b7b6ef44dc97d8c6
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-05-07 16:49:56 -0300

    Codificando com bloco de notas

---
+++ CodigosDiversos/PrimeiroProgama.java
@@ -0,0 +1,14 @@
+public class PrimeiroProgama{
+	public static void main(String[] args){
+
+		System.out.println("Usando o bloco de notas");
+		int numero = 10;
+		int numero2 = 20;
+
+		System.out.println(numero + numero2);
+			
+	}
+
+		
+
+}
