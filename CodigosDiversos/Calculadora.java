66a3e63ddd6b1212c7549bd5dc51a93d32eb742c
Richardson Rodrigues <richardsonrodrigues.dev@gmail.com>
2022-05-09 15:52:45 -0300

    Add calculadora

---
+++ CodigosDiversos/Calculadora.java
@@ -0,0 +1,28 @@
+public class Calculadora {
+
+
+
+    public static double add (double number1, double number2){
+
+        return number1 + number2;
+
+    }
+
+
+    public static double subtract(double number1, double number2){
+
+        return number1 - number2;
+
+    }
+
+
+    public static double multiply(double number1, double number2){
+
+        return number1 * number2;
+    }
+
+    public static double division(double number1, double number2){
+        return number1 / number2;
+    }
+
+}
---
+++ CodigosDiversos/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void  main (String[] args){
+
+        System.out.println(Calculadora.add(12,34));
+        System.out.println(Calculadora.subtract(12,3.5));
+        System.out.println(Calculadora.multiply(145,3));
+        System.out.println(Calculadora.division(15,3));
+
+        
+
+
+    }
+
+
+
+}
+
